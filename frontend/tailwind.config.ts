4d49124c06250b57b36e741a25ac012cce3826d8
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2025-10-01 16:53:44 +0100

    config: add scrollbar-hide utility to Tailwind config

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -14,5 +14,17 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }: { addUtilities: Function }) {
+      addUtilities({
+        ".scrollbar-hide": {
+          "-ms-overflow-style": "none",
+          "scrollbar-width": "none",
+          "&::-webkit-scrollbar": {
+            display: "none",
+          },
+        },
+      });
+    },
+  ],
 } satisfies Config;
